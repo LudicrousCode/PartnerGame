8992f508e68376bf77bf28b7bd7be1cd54eb3807
rachel_chau <rachel_chau@student.wayland.k12.ma.us>
2017-03-15 10:15:16 -0400

    added Sprite class

--- AGame/src/Player.java
+++ AGame/src/Player.java
@@ -1,21 +1,37 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 /**
  * Created by rachel_chau on 3/15/17.
  */
 public class Player {
 
-    private int x, y;
-
-    public Player(){
-
-
-
-    }
-
-    public void draw(Graphics2D g2){
-
-
-    }
+//    private int x, y;
+//    private BufferedImage pic; //put the file in the res folder.
+//    private int dir, picOrientation; //dir is the current direction in degrees.  See the constants below.
+//    public static final int NORTH = 90, SOUTH = 270, WEST = 180, EAST = 0, NE = 45, NW = 135, SW = 225, SE = 315;
+//
+//
+//    public Player(){
+//
+//        setPic("player.png", NORTH);
+//
+//    }
+//
+//    public void draw(Graphics2D g2){
+//
+//
+//    }
+//
+//    public void setPic(String fileName, int orientation) {
+//        try {
+//            pic = ImageIO.read(new File("res/" + fileName));
+//            picOrientation = orientation;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
