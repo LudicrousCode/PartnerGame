45ccf93c52da50a45b487517c4f007c0c39a7af7
LudicrousCode <andrew_briasco-stewart@student.wayland.k12.ma.us>
2017-03-15 10:00:16 -0400

    Set up of main

--- AGame/src/Main.java
+++ AGame/src/Main.java
@@ -1,11 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 /**
  * Created by andrew_briasco on 3/14/17.
  */
-public class Main {
+public class Main extends JPanel {
+    public static final int FRAMEWIDTH = 500, FRAMEHEIGHT = 800;
+
+    public Main() {
+    }
+
+
+
+
+
+
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.drawRect(FRAMEWIDTH-2, FRAMEHEIGHT-2, 2, 2);
+    }
+
+
+
     public static void main(String[] args) {
+        JFrame window = new JFrame("Jumper");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(0, 0, FRAMEWIDTH, FRAMEHEIGHT + 22); //(x, y, w, h) 22 due to title bar.
 
-        System.out.println("Test");
-        System.out.println("Hi Andrew");
+        Main panel = new Main();
+        panel.setSize(FRAMEWIDTH, FRAMEHEIGHT);
 
+//        panel.setFocusable(true);
+        panel.grabFocus();
+//        window.setLocationRelativeTo(null);
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
     }
 }
