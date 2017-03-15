cca398931cca808ec506a9ecd99777770a18584f
rachel_chau <rachel_chau@student.wayland.k12.ma.us>
2017-03-15 09:41:54 -0400

    Merge branch 'master' of /Users/rachel_chau/Desktop/PartnerGame with conflicts.

--- AGame/src/Main.java
+++ AGame/src/Main.java
@@ -3,7 +3,9 @@
  */
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Test");
-        System.out.println("Hi?");
+        System.out.println("Hi Andrew");
+
     }
 }
